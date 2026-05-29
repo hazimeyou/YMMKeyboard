20f2a2dd5d22ef9cdc08ff402d02b22d6bef4861
hazimeyou <125953620+hazimeyou@users.noreply.github.com>
2026-05-30 00:27:10 +0900

    シーク操作をCommandSettings Seek Invoke経由へ変更し直接更新をフォールバック化

--- YMMKeyboardPlugin/Actions/KeyboardAction.cs
+++ YMMKeyboardPlugin/Actions/KeyboardAction.cs
@@ -29,24 +29,12 @@ namespace YMMKeyboardPlugin.Actions
 
         public static void PlusSeekFrame(int frames)
         {
-            if (TimelineInstance is null)
-                return;
-
-            if (Application.Current?.Dispatcher is { } dispatcher)
-                dispatcher.Invoke(() => TimelineInstance.CurrentFrame += frames);
-            else
-                TimelineInstance.CurrentFrame += frames;
+            SeekByFrames(frames);
         }
 
         public static void MinusSeekFrame(int frames)
         {
-            if (TimelineInstance is null)
-                return;
-
-            if (Application.Current?.Dispatcher is { } dispatcher)
-                dispatcher.Invoke(() => TimelineInstance.CurrentFrame -= frames);
-            else
-                TimelineInstance.CurrentFrame -= frames;
+            SeekByFrames(-frames);
         }
 
         public static void PlusFrame(int frames)
@@ -57,6 +45,75 @@ namespace YMMKeyboardPlugin.Actions
         public static void MinusFrame(int frames)
         {
             MinusSeekFrame(frames);
+        }
+
+        private static void SeekByFrames(int frames)
+        {
+            if (frames == 0)
+                return;
+
+            var timeline = TimelineInstance;
+            if (timeline is null)
+                return;
+
+            void Action()
+            {
+                if (!TryInvokeSeekCommand(frames))
+                    timeline.CurrentFrame += frames;
+            }
+
+            if (Application.Current?.Dispatcher is { } dispatcher)
+                dispatcher.Invoke(Action);
+            else
+                Action();
+        }
+
+        private static bool TryInvokeSeekCommand(int frameDelta)
+        {
+            try
+            {
+                // YMM API: CommandSettings.Default["Seek"].Invoke(frameDelta or TimeSpan)
+                var commandSettingsType = Type.GetType("YukkuriMovieMaker.Commons.CommandSettings, YukkuriMovieMaker");
+                if (commandSettingsType is null)
+                    return false;
+
+                var defaultProperty = commandSettingsType.GetProperty("Default", BindingFlags.Public | BindingFlags.Static);
+                var settingsInstance = defaultProperty?.GetValue(null);
+                if (settingsInstance is null)
+                    return false;
+
+                var indexer = commandSettingsType.GetProperty("Item", BindingFlags.Public | BindingFlags.Instance, null, null, new[] { typeof(string) }, null);
+                var seekCommand = indexer?.GetValue(settingsInstance, new object[] { "Seek" });
+                if (seekCommand is null)
+                    return false;
+
+                var invokeMethod = seekCommand.GetType().GetMethod("Invoke", new[] { typeof(object) })
+                    ?? seekCommand.GetType().GetMethod("Invoke", new[] { typeof(int) })
+                    ?? seekCommand.GetType().GetMethod("Invoke", new[] { typeof(TimeSpan) });
+                if (invokeMethod is null)
+                    return false;
+
+                var parameterType = invokeMethod.GetParameters().FirstOrDefault()?.ParameterType;
+                if (parameterType == typeof(TimeSpan))
+                {
+                    invokeMethod.Invoke(seekCommand, new object[] { TimeSpan.FromMilliseconds(frameDelta) });
+                }
+                else if (parameterType == typeof(int))
+                {
+                    invokeMethod.Invoke(seekCommand, new object[] { frameDelta });
+                }
+                else
+                {
+                    invokeMethod.Invoke(seekCommand, new object[] { frameDelta });
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"[KeyboardAction] Seek command invoke failed: {ex.Message}");
+                return false;
+            }
         }
 
 #pragma warning disable CS0067
